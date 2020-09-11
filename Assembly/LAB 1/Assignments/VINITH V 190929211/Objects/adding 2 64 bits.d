1d16964992cbe014bd9a8e52d5c98c25b0276878
VINITH V <70522076+DevilHunterV@users.noreply.github.com>
2020-09-11 13:35:05 +0530

    Vinith.V

---
+++ Assembly/LAB 1/Assignments/VINITH V 190929211/Objects/adding 2 64 bits.d
@@ -0,0 +1,3 @@
+.\objects\adding 2 64 bits.o: ..\..\New folder\Adding 2 64 bits.s
+.\objects\adding 2 64 bits.o: ..\..\New folder\MSP432xx_constants.s
+.\objects\adding 2 64 bits.o: ..\..\New folder\MSP432xx_tim_constants.s
---
+++ Assembly/LAB 1/Assignments/VINITH V 190929211/Objects/subtracting 64 bits.d
@@ -0,0 +1,3 @@
+.\objects\subtracting 64 bits.o: Subtracting 64 bits.s
+.\objects\subtracting 64 bits.o: MSP432xx_constants.s
+.\objects\subtracting 64 bits.o: MSP432xx_tim_constants.s
