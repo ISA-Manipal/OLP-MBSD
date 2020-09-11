55d8eb3abe0d31be8365066d16450f2bbefd0b05
Tejal VS <60037866+MasterTevs@users.noreply.github.com>
2020-09-11 20:28:29 +0530

    Revert "Merge branch 'master' of https://github.com/ISA-Manipal/OLP-MBSD"
    This reverts commit 1faca61decd79f14935693baf67a97ec6df458bf, reversing
    changes made to 3c234b85a5140d274ce259536bbcf37d6d2f8b64.

--- Assembly/LAB 1/Assignments/Karthikgogisetty/Subtract64bit/Objects/subtract2_64bit_nums.d
+++
@@ -1,3 +0,0 @@
-.\objects\subtract2_64bit_nums.o: subtract2_64bit_nums.s
-.\objects\subtract2_64bit_nums.o: MSP432xx_constants.s
-.\objects\subtract2_64bit_nums.o: MSP432xx_tim_constants.s
--- Assembly/LAB 1/Assignments/Karthikgogisetty/probStatement/Objects/revsub_suresh.d
+++
@@ -1,3 +0,0 @@
-.\objects\revsub_suresh.o: revsub_suresh.s
-.\objects\revsub_suresh.o: MSP432xx_constants.s
-.\objects\revsub_suresh.o: MSP432xx_tim_constants.s
